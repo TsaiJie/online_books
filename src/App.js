1cb1ab38a4037111b48d65bbb41e3ed9f793827f
tsai <mengjietsai@163.com>
2020-12-27 22:09:45 +0800

    PriceList基本的布局

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PriceList from './components/PriceList';
+const items = [
+  {
+    id: 1,
+    title: '去云南旅游',
+    date: '2018-09-10',
+    price: 200,
+    category: {
+      id: '1',
+      name: '旅行',
+      type: 'outcome',
+    },
+  },
+  {
+    id: 2,
+    title: '去云南旅游',
+    date: '2018-09-10',
+    price: 400,
+    category: {
+      id: '1',
+      name: '旅行',
+      type: 'outcome',
+    },
+  },
+  
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -14,8 +39,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <PriceList 
+        items={items}
+        onModifyItem={(item) => {console.log(item)}}
+        onDeleteItem={(item) => {console.log(item)}}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

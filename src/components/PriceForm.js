e68bfd7b5d47efe300dd98682f3552d4e230acc0
tsai <mengjietsai@163.com>
2020-12-30 22:01:09 +0800

    PriceFrom 基本结构

---
+++ src/components/PriceForm.js
@@ -0,0 +1,61 @@
+import PropTypes from 'prop-types'
+import { isValidDate } from '../utility'
+
+import React, { PureComponent } from 'react'
+
+export default class PriceForm extends PureComponent {
+  static propTypes = {
+    onFormSubmit: PropTypes.func.isRequired,
+    onCancelSubmit: PropTypes.func.isRequired,
+    item: PropTypes.object,
+  }
+  static defaultProps = {
+    item: {}
+  }
+  state = {
+    validatePass: true,
+    errorMessage: '',
+  }
+  render() {
+    const {title, date, price} = this.props.item
+    return (
+      <form noValidate>
+        <div className="form-group">
+          <label htmlFor="title">标题*</label>
+          <input 
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="请输入标题"
+            defaultValue={title}
+            ref={(input) => {this.titleInput = input}}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">价格*</label>
+          <input 
+            type="number"
+            className="form-control"
+            id="price"
+            placeholder="请输入价格"
+            defaultValue={price}
+            ref={(input) => {this.PriceInput = input}}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">日期*</label>
+          <input 
+            type="date"
+            className="form-control"
+            id="date"
+            placeholder="请输入日期"
+            defaultValue={date}
+            ref={(input) => {this.dateInput = input}}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mr-3">提交</button>
+        <button className="btn btn-secondary" onClick={this.props.onCancelSubmit}> 取消 </button>
+      </form>
+    )
+  }
+}
--- src/containers/Create.js
+++ src/containers/Create.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CategorySelect from '../components/CategorySelect'
+import PriceForm from '../components/PriceForm'
 export const categories = [
   {
     id: '1',
@@ -20,6 +21,26 @@ export const categories = [
     iconName: 'logo-yen'
   },
 ]
+const testItem = [
+  {
+    "title": "这是我的工资",
+    "price": 20000,
+    "date": "2018-08-18",
+    "monthCategory": "2018-8",
+    "id": "_bd16bjeen",
+    "cid": "2",
+    "timestamp": 1534550400000
+  },
+  {
+    "title": "buy stuff for kitten",
+    "price": 100,
+    "date": "2018-08-15",
+    "monthCategory": "2018-8",
+    "id": "_kly1klf4g",
+    "cid": "1",
+    "timestamp": 1534291200000
+  }
+]
 let propsWithCategory = {
   categories,
   onSelectCategory: (categroy)=>{console.log(categroy)},
@@ -27,6 +48,12 @@ let propsWithCategory = {
 }
 const Create = ({match }) => {
   console.log(match)
-  return <CategorySelect {...propsWithCategory}/>
+  return (
+    <React.Fragment>
+      <CategorySelect {...propsWithCategory}/>
+      <PriceForm item={testItem[0]} onFormSubmit={()=>{}} onFormCreate={()=>{}}/>
+    </React.Fragment>
+
+  )
 }
 export default Create

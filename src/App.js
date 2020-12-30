35700fd14083094672ba641814ec9a021fab3b89
tsai <mengjietsai@163.com>
2020-12-30 18:44:36 +0800

    React Router的使用

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './containers/Home'
+import Create from './containers/Create'
 
 
 function App(params) {
  return (
-   <Home />
+   <Router>
+    <div className="App">
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/create'>Create</Link>
+        <Link to='/edit/10'>Edit</Link>
+      </ul>
+      <div className="container pb-5">
+        {/* exact 是路径完全匹配 */}
+        <Route path='/' exact component={Home} />
+        <Route path='/create' component={Create} />
+        <Route path='/edit/:id' component={Create} />
+      </div>
+    </div>
+   </Router>
  ) 
 }
 export default App
---
+++ src/containers/Create.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const Create = ({match }) => {
+  console.log(match)
+  return <h1>This is the create page</h1>
+}
+export default Create

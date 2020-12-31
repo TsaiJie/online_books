bea257fd2d7651c1cd1b414184741b48024a4a13
tsai <mengjietsai@163.com>
2020-12-31 18:30:32 +0800

    create页面 tab切换功能

--- src/containers/Create.js
+++ src/containers/Create.js
@@ -6,24 +6,37 @@ import { testCategories } from '../testData'
 import { TYPE_INCOME, TYPE_OUTCOME } from '../utility'
 import { AppContext } from '../App'
 import withContext from '../WithContext'
+const tabsText = [TYPE_OUTCOME, TYPE_INCOME]
 class Create extends PureComponent {
+  state = {
+    selectedTab: TYPE_OUTCOME,
+    selectedCategory: null,
+  }
+  tabChange = (index) => {
+    this.setState({
+      selectedTab: tabsText[index],
+    })
+  }
   render() {
     const { data } = this.props
-    const filterCategories = testCategories.filter(
-      (categroy) => categroy.type === TYPE_OUTCOME
-    )
+    const { items, categories } = data
+    const { selectedTab } = this.state
+    const filterCategories = Object.keys(categories)
+    .filter(id => categories[id].type === selectedTab)
+    .map(id => categories[id])
+
     return (
       <div
         className="create-page py-3 px-3 rounded mt-3"
         style={{ background: '#fff' }}
       >
-        <Tabs activeIndex={0} onTabChange={() => {}}>
+        <Tabs activeIndex={0} onTabChange={this.tabChange}>
           <Tab>支出</Tab>
           <Tab>收入</Tab>
         </Tabs>
         <CategorySelect
           categories={filterCategories}
-          onSelectCategory={() => {}}
+          onSelectCategory={(e) => {console.log(e)}}
         />
         <PriceForm
           onFormSubmit={(a, b) => {

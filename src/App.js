dcd950b3bc1ac66e740b26109a8573d99d934b1f
tsai <mengjietsai@163.com>
2021-01-03 16:39:43 +0800

    删除APP中的多余结构

--- src/App.js
+++ src/App.js
@@ -143,11 +143,6 @@ class App extends PureComponent {
       >
         <Router>
           <div className="App">
-            <ul>
-              <Link to="/">Home</Link>
-              <Link to="/create">Create</Link>
-              <Link to="/edit/10">Edit</Link>
-            </ul>
             <div className="container pb-5">
               {/* exact 是路径完全匹配 */}
               <Route path="/" exact component={Home} />
